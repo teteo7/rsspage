e5d3fd350900005ee220f15a9731f22ce5224b36
teteo7 <teteocomputer@gmail.com>
2022-06-18 07:54:32 +0900

    soft commit

--- rss/src/main/java/rssproject/rss/controller/indexController.java
+++ rss/src/main/java/rssproject/rss/controller/indexController.java
@@ -1,7 +1,24 @@
 package rssproject.rss.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class indexController {
+
+    @GetMapping("apple")
+    public String apple(Model model){
+        model.addAttribute("data", "apple");
+        return "apple";
+    }
+
+//    URL에 param값 넣었는데 안되
+    @GetMapping("list")
+    @ResponseBody
+    public String list(@RequestParam("param") String param, Model model){
+        return "list"+param;
+    }
 }
